5eac714295b775e70526cf52910871dd265b6304
Goshitha Premachandra <wkgoshithapremachandra@gmail.com>
2024-08-19 02:39:34 +0530

    Connect project with MongoDB

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const express = require('express')
+const express = require('express') 
+const { default: mongoose } = require('mongoose');
 
 const app = express();
+
 require('dotenv').config();
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('server start!')
   })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+mongoose.connect("mongodb://127.0.0.1:27017/Final_Project_DB")
+  .then(() => {
+    console.log("Connected successfully to the MongoDB database");
   })
+  .catch((error) => {
+    console.error("An error occurred while connecting to MongoDB:", error);
+  });
+
+try {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Failed to start the server:", error);
+}
