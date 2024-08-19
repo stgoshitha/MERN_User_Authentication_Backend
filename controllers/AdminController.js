10c41e152a91fd7c92430be9c7640eb386d196ec
Goshitha Premachandra <wkgoshithapremachandra@gmail.com>
2024-08-20 01:22:45 +0530

    implement getuser and deletuser functions

---
+++ controllers/AdminController.js
@@ -0,0 +1,39 @@
+const userModel = require("../models/UserModel");
+
+const getUser = async (req, res) => {
+    try {
+        const users = await userModel.find();
+        res.status(200).json({users})
+
+    } catch (err) {
+        res.status(500).json({massage:'intenral server error'});
+        console.log(err);
+    }
+};
+
+
+
+const deletUser = async(req,res) =>{
+    try {
+        const userId = req.params.id
+
+        const checkAdmin = await userModel.findById(userId)
+
+        if(checkAdmin.role == 'admin'){
+            res.status(401).json({massage:'you can not delet admin'})
+        }
+
+        const user = await userModel.findByIdAndDelete(userId)
+
+        if(!user){
+            res.status(404).json({massage:'user not found'})
+        }
+        res.status(200).json({massage:'user delet successfully'})
+
+    } catch (err) {
+        res.status(500).json({massage:'intenral server error'});
+        console.log(err);
+    }
+}
+
+module.exports = {getUser, deletUser};
